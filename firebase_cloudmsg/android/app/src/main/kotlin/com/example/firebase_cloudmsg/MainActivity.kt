cd59c9937b8f068d025bd600ea331d3d27f35e57
Quang Tran <qdtran.dev@gmail.com>
2026-04-14 02:47:59 -0400

    feat: add firebase_cloudmsg project with Firebase and FCM dependencies

---
+++ firebase_cloudmsg/android/app/src/main/kotlin/com/example/firebase_cloudmsg/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.firebase_cloudmsg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ firebase_cloudmsg/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'screens/home_screen.dart';
+
+@pragma('vm:entry-point')
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  debugPrint('Background message: ${message.messageId}');
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'FCM Demo',
+      home: HomeScreen(),
+    );
+  }
+}
